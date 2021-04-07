a31d68c88827efb3c3f61960d4db89c84de3e4a1
Aravinda Vishwanathapura <aravinda@kadalu.io>
2021-04-07 21:59:18 +0530

    Tailwindcss setup
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.io>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,48 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [
+        './layouts/**/*.html',
+        './content/**/*.html',
+        './content/**/*.erb',
+        './content/**/*.md',
+        './content/**/*.adoc'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    variants: {
+        extend: {},
+    },
+    theme: {
+        extend: {
+            typography: (theme) => ({
+                DEFAULT: {
+                    css: {
+                        pre: {
+                            color: theme("colors.gray.900"),
+                            backgroundColor: '#f3f6fa'
+                        },
+                        "pre code::before": {
+                            "padding-left": "unset"
+                        },
+                        "pre code::after": {
+                            "padding-right": "unset"
+                        },
+                        code: {
+                            backgroundColor: '#f3f6fa',
+                            color: "#DD1144",
+                            fontWeight: "400",
+                            "border-radius": "0.25rem"
+                        },
+                        "code::before": {
+                            content: '""',
+                            "padding-left": "0.25rem"
+                        },
+                        "code::after": {
+                            content: '""',
+                            "padding-right": "0.25rem"
+                        }
+                    }
+                }
+            })
+        }
+    },
+    plugins: [require("@tailwindcss/typography")],
 }
